e3ada511a4b5be0e3f2c70151f462621a0835924
zakogoto <116558645+zakogoto@users.noreply.github.com>
2023-02-19 01:02:17 +0400

    almost finish layout

---
+++ dist/js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const burger = document.querySelector('.burger'),
+      menu = document.querySelector('.float-menu'),
+      close = document.querySelector('.float-menu__close');
+
+burger.addEventListener('click', ()=> {
+menu.classList.add('active');
+});
+
+close.addEventListener('click', ()=> {
+    menu.classList.remove('active');
+});
