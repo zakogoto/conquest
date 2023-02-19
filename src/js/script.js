5492aeaf5dea0582297fc60fb02aca09ce29d013
zakogoto <116558645+zakogoto@users.noreply.github.com>
2023-02-19 19:30:09 +0400

    add menu events

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,27 @@
 
 const burger = document.querySelector('.burger'),
       menu = document.querySelector('.float-menu'),
+      menuItems = document.querySelectorAll('.float-menu__item'),
+      overlay = document.querySelector('.float-menu__overlay'),
       close = document.querySelector('.float-menu__close');
 
 burger.addEventListener('click', ()=> {
     menu.classList.add('active');
 });
 
+function closeMenu(target) {
+    target.classList.remove('active');
+}
+
 close.addEventListener('click', ()=> {
-    menu.classList.remove('active');
+    closeMenu(menu);
 });
+
+overlay.addEventListener('click', ()=> {
+    closeMenu(menu);
+});
+menuItems.forEach(item  => {
+    item.addEventListener('click', ()=> {
+        closeMenu(menu);
+    })
+})
